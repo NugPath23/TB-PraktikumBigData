a1ab5834b21588b65d98acbef2a67134efd1d6e6
NUnug <146148793+NugPath23@users.noreply.github.com>
2025-12-26 03:07:16 +0700

    Update app.py

--- project_rumah_tebet/app.py
+++ project_rumah_tebet/app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1aeJ1Dv5t0NyApvXjaGgTr0ajfmvGySXp
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import joblib
